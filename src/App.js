17e91622eb8d10a52350dc88273b18be57ed3d56
TDCIAN <tdcian71@gmail.com>
2022-07-11 07:27:18 +0900

    add config basic structure of webpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Box>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
